293817054e899abcfb7b4854d233cbc3e8a81eee
Andres Suarez <andresfelipe9619@gmail.com>
2021-05-24 06:41:25 -0500

    :building_construction: Use remote DB and env vars

--- api/config/database.js
+++ api/config/database.js
@@ -4,17 +4,23 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'sentir-creativo'),
-        username: env('DATABASE_USERNAME', 'mongoadmin'),
-        password: env('DATABASE_PASSWORD', 'secret'),
+        uri: env('DATABASE_URI')
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-    },
-  },
-});
+        ssl: true
+      }
+      // settings: {
+      //   host: env('DATABASE_HOST', '127.0.0.1'),
+      //   srv: env.bool('DATABASE_SRV', false),
+      //   port: env.int('DATABASE_PORT', 27017),
+      //   database: env('DATABASE_NAME', 'sentir-creativo'),
+      //   username: env('DATABASE_USERNAME', 'mongoadmin'),
+      //   password: env('DATABASE_PASSWORD', 'secret'),
+      // },
+      // options: {
+      //   authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
+      //   ssl: env.bool('DATABASE_SSL', false),
+      // },
+    }
+  }
+})
