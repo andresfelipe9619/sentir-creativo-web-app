b4eca591012d83f89e7288ca3670afe43c46246c
Andres Suarez <andresfelipe9619@gmail.com>
2021-07-16 14:29:42 -0500

    :sparkles: Allow creating audience

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import clsx from 'clsx'
+import { useTheme } from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import List from '@material-ui/core/List'
-import CssBaseline from '@material-ui/core/CssBaseline'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
 import HomeIcon from '@material-ui/icons/HomeOutlined'
 import PersonIcon from '@material-ui/icons/PersonOutlined'
-import GamepadIcon from '@material-ui/icons/GamepadOutlined'
-import WorkIcon from '@material-ui/icons/WorkOutlineOutlined'
+import BookIcon from '@material-ui/icons/Book'
 import EmailIcon from '@material-ui/icons/EmailOutlined'
-import ListItem from '@material-ui/core/ListItem'
 import { useHistory, useLocation } from 'react-router-dom'
 import { useStyles } from './styles'
 
-export default function Sidebar ({ children, toggleDarkMode }) {
+export default function Sidebar ({ children }) {
   const classes = useStyles()
   const history = useHistory()
   const location = useLocation()
+  const theme = useTheme()
+  const [open, setOpen] = useState(false)
+
+  const handleDrawerOpen = () => setOpen(true)
+
+  const handleDrawerClose = () => setOpen(false)
 
   const goTo = path => () => history.push(path)
   const isSelected = path => location.pathname === path
@@ -27,19 +39,46 @@ export default function Sidebar ({ children, toggleDarkMode }) {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position='fixed' className={classes.appBar}>
+      <AppBar
+        position='fixed'
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open
+        })}
+      >
         <Toolbar>
-          <div className={classes.grow}>Sentir Creativo</div>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' noWrap>
+            Sentir Creativo
+          </Typography>
         </Toolbar>
       </AppBar>
       <Drawer
-        variant='permanent'
-        className={clsx(classes.drawer, classes.drawerClose)}
+        className={classes.drawer}
+        variant='persistent'
+        anchor='left'
+        open={open}
         classes={{
-          paper: classes.drawerClose,
-          paperAnchorLeft: classes.paperAnchorLeft
+          paper: classes.drawerPaper
         }}
       >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
         <List>
           {options.map(({ name, path, icon: Icon }, index) => (
             <ListItem
@@ -56,8 +95,12 @@ export default function Sidebar ({ children, toggleDarkMode }) {
           ))}
         </List>
       </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open
+        })}
+      >
+        <div className={classes.drawerHeader} />
         {children}
       </main>
     </div>
@@ -78,7 +121,7 @@ const options = [
   {
     path: '/blog',
     name: 'Blog',
-    icon: GamepadIcon
+    icon: BookIcon
   },
   {
     path: '/contact',
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import API from '../api'
+import Card from '../components/card/Card'
 
-export default function Home() {
-    return (
-        <div>
-            HOME
-        </div>
-    )
+export default function Home () {
+  const [services, setServices] = useState([])
+  const classes = useStyles()
+  useEffect(() => {
+    ;(async () => {
+      const result = await API.Service.getAll()
+      setServices(result)
+    })()
+  }, [])
+  console.log(`services`, services)
+  return (
+    <Box mt={16}>
+      <Typography className={classes.title} variant='h2' gutterBottom>
+        Sentir Creativo{' '}
+        <Typography
+          variant='h2'
+          component='span'
+          color='primary'
+          className={classes.title}
+        >
+          ¡Anímate!
+        </Typography>
+      </Typography>
+      <Typography variant='h5' gutterBottom color='textSecondary' paragraph>
+        Agencia de Talentos en Antofagasta Abierto las 24 horas.
+      </Typography>
+      <Button variant='contained' color='primary'>
+        Contactanos
+      </Button>
+      <Box mt={8}>
+        {services.map(s => (
+          <Card
+            key={s.nombre}
+            title={s.nombre}
+            imageUrl={
+              'https://lh3.googleusercontent.com/MBD0jOYrgUVHTZIvHgHoZJUWyAwXrL-VVuJ-hhvG_c--qKkfVkS0VFuId8wU57ChBZO8IsJ2aOY6ueEByg=w960-h960-n-o-v1'
+            }
+            imageTitle={''}
+            sintesis={s.sintesis}
+            slogan={s.slogan}
+          />
+        ))}
+      </Box>
+    </Box>
+  )
 }
+
+export const useStyles = makeStyles(theme => ({
+  title: { fontWeight: 'bold' }
+}))
