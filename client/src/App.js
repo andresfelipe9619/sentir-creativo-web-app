92b8329dd8a9f3a118d21c4cf670ee8d1be4b5d1
Andres Suarez <andresfelipe9619@gmail.com>
2021-08-01 18:37:14 -0500

    :rocket: Add more models

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles'
 import { useDarkMode } from './theme'
 import Sidebar from './components/sidebar/Sidebar'
 import RouterApp from './router/router.app'
 
 function App () {
   const [theme] = useDarkMode()
-  const themeConfig = createMuiTheme(theme)
+  const themeConfig = createTheme(theme)
 
   return (
     <MuiThemeProvider theme={themeConfig}>
