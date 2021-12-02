60e63d575a3e160ec4e6ec4e3d6891618da7d1f4
Andres Suarez <andresfelipe9619@gmail.com>
2021-12-01 20:51:57 -0500

    :recycle: Reuse files services

--- server/api/coleccion/controllers/coleccion.js
+++ server/api/coleccion/controllers/coleccion.js
@@ -1,8 +1,20 @@
-'use strict';
+'use strict'
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async addFiles (ctx) {
+    const { request, params } = ctx
+    const { id } = params
+    const { body: files } = request
+    const result = await strapi.services.archivo.addFiles({
+      id,
+      files,
+      collection: 'coleccion'
+    })
+    return result
+  }
+}
