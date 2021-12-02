0f827a152b699129dfaaf61d5aa74baa8dd4bac3
Andres Suarez <andresfelipe9619@gmail.com>
2021-12-01 20:51:57 -0500

    :recycle: Reuse files services

--- api/servicio/controllers/servicio.js
+++ api/servicio/controllers/servicio.js
@@ -38,29 +38,12 @@ module.exports = {
   async addFiles (ctx) {
     const { request, params } = ctx
     const { id } = params
-    let { body: files } = request
-    const entity = await strapi.services.servicio.findOne({ id }, populate)
-    let ids = []
-    if (!entity) throw new Error('Servicio No Encontrado')
-    if (!Array.isArray(files)) throw new Error('No hay archivos para agregar')
-    let serviceFiles = (entity.archivos || []).map(({ id }) => id)
-    files = files.filter(f => !serviceFiles.includes(f))
-    const knex = strapi.connections.default
-    await knex.transaction(async trx => {
-      ids = await trx('servicios__archivos').insert(
-        files.map(fileId => ({
-          servicio_id: id,
-          archivo_id: fileId
-        }))
-      )
+    const { body: files } = request
+    const result = await strapi.services.archivo.addFiles({
+      id,
+      files,
+      collection: 'servicio'
     })
-    let result = []
-    if (ids.length) {
-      result = await knex
-        .select('*')
-        .from('servicios__archivos')
-        .whereIn('id', ids)
-    }
     return result
   }
 }
