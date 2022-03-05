33ad750189b40fb2424dc6bee53a1bd3278deb18
Andres Suarez <andresfelipe9619@gmail.com>
2022-03-05 06:28:33 -0500

    :sparkles: Add more staff columns

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -115,6 +115,14 @@ const AudienceState = {
   delete: id => serverRequests.del(`/estado-audiencias/${id}`)
 }
 
+const StaffState = {
+  getAll: () => serverRequests.get(`/estado-staffs`),
+  get: id => serverRequests.get(`/estado-staffs/${id}`),
+  create: state => serverRequests.post('/estado-staffs', state),
+  update: (id, state) => serverRequests.put(`/estado-staffs/${id}`, state),
+  delete: id => serverRequests.del(`/estado-staffs/${id}`)
+}
+
 const PublicoObjetivo = {
   getAll: () => serverRequests.get(`/publico-objetivos`),
   get: id => serverRequests.get(`/publico-objetivos/${id}`),
@@ -199,25 +207,33 @@ const Antiguedad = {
 const CuponDescuento = {
   getAll: () => serverRequests.get(`/cupon-descuentos`),
   get: id => serverRequests.get(`/cupon-descuentos/${id}`),
-  create: format => serverRequests.post('/cupon-descuentos', format),
-  update: (id, format) => serverRequests.put(`/cupon-descuentos/${id}`, format),
+  create: cupon => serverRequests.post('/cupon-descuentos', cupon),
+  update: (id, cupon) => serverRequests.put(`/cupon-descuentos/${id}`, cupon),
   delete: id => serverRequests.del(`/cupon-descuentos/${id}`)
 }
 
 const Cercania = {
   getAll: () => serverRequests.get(`/cercanias`),
   get: id => serverRequests.get(`/cercanias/${id}`),
-  create: format => serverRequests.post('/cercanias', format),
-  update: (id, format) => serverRequests.put(`/cercanias/${id}`, format),
+  create: cercania => serverRequests.post('/cercanias', cercania),
+  update: (id, cercania) => serverRequests.put(`/cercanias/${id}`, cercania),
   delete: id => serverRequests.del(`/cercanias/${id}`)
 }
 
 const Origen = {
   getAll: () => serverRequests.get(`/origens`),
   get: id => serverRequests.get(`/origens/${id}`),
-  create: format => serverRequests.post('/origens', format),
-  update: (id, format) => serverRequests.put(`/origens/${id}`, format),
+  create: origen => serverRequests.post('/origens', origen),
+  update: (id, origen) => serverRequests.put(`/origens/${id}`, origen),
   delete: id => serverRequests.del(`/origens/${id}`)
+}
+
+const Acuerdo = {
+  getAll: () => serverRequests.get(`/acuerdos`),
+  get: id => serverRequests.get(`/acuerdos/${id}`),
+  create: acuerdo => serverRequests.post('/acuerdos', acuerdo),
+  update: (id, acuerdo) => serverRequests.put(`/acuerdos/${id}`, acuerdo),
+  delete: id => serverRequests.del(`/acuerdos/${id}`)
 }
 
 const TipoArchivo = {
@@ -228,33 +244,45 @@ const TipoArchivo = {
   delete: id => serverRequests.del(`/tipo-archivos/${id}`)
 }
 
+const TecnicaArtistica = {
+  getAll: () => serverRequests.get(`/tecnica-artisticas`),
+  get: id => serverRequests.get(`/tecnica-artisticas/${id}`),
+  create: tecnica => serverRequests.post('/tecnica-artisticas', tecnica),
+  update: (id, tecnica) =>
+    serverRequests.put(`/tecnica-artisticas/${id}`, tecnica),
+  delete: id => serverRequests.del(`/tecnica-artisticas/${id}`)
+}
+
 const API = {
-  Auth,
-  User,
-  Area,
-  Tag,
-  Rubro,
-  Archivo,
-  Proyecto,
-  Audiencia,
-  Servicio,
-  Formato,
-  Prefijo,
-  Staf,
-  TipoArchivo,
-  Motivacion,
+  Acuerdo,
   Antiguedad,
-  CuponDescuento,
-  Cercania,
-  Origen,
-  Coleccion,
+  Archivo,
+  Area,
   AudienceState,
-  ServiceState,
-  PublicoObjetivo,
+  Audiencia,
+  Auth,
+  Cercania,
+  Coleccion,
+  CuponDescuento,
   EstadoProyecto,
+  Formato,
+  Motivacion,
+  Origen,
+  Prefijo,
+  Proyecto,
+  PublicoObjetivo,
+  Rubro,
+  ServiceState,
+  Servicio,
+  Staf,
+  StaffState,
+  Tag,
+  TecnicaArtistica,
+  TipoArchivo,
   TipoProyecto,
-  setToken,
-  getToken
+  User,
+  getToken,
+  setToken
 }
 
 export default API
