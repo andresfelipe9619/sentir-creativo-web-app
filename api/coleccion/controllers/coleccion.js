d79c70dea55300ff02b0877ae657d58554beaecb
Andres Suarez <andresfelipe9619@gmail.com>
2022-03-05 22:39:22 -0500

    :bug: Populate files

--- api/coleccion/controllers/coleccion.js
+++ api/coleccion/controllers/coleccion.js
@@ -1,11 +1,30 @@
-'use strict'
-
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require('strapi-utils')
+const populate = [
+  'id',
+  'archivos',
+  'servicios',
+  'cuponDescuento',
+  'archivos.tipo_archivo'
+]
 
 module.exports = {
+  async find (ctx) {
+    let entities
+    if (ctx.query._q) {
+      entities = await strapi.services.coleccion.search(ctx.query)
+    } else {
+      entities = await strapi.services.coleccion.find(ctx.query, populate)
+    }
+
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models.coleccion })
+    )
+  },
+  async findOne (ctx) {
+    const { id } = ctx.params
+    const entity = await strapi.services.coleccion.findOne({ id }, populate)
+    return sanitizeEntity(entity, { model: strapi.models.coleccion })
+  },
   async addFiles (ctx) {
     const { request, params } = ctx
     const { id } = params
