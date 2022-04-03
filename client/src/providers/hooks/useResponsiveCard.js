e512644ca18f8d9170f3e4e4728a58608d791596
Andres Suarez <andresfelipe9619@gmail.com>
2022-04-03 09:43:27 -0500

    :recycle: Reuse code

--- client/src/providers/hooks/useResponsiveCard.js
+++ client/src/providers/hooks/useResponsiveCard.js
@@ -5,7 +5,7 @@ export default function useResponsiveCard() {
   const theme = useTheme();
   const isSmall = useMediaQuery(theme.breakpoints.down("xs"));
   const isMedium = useMediaQuery(theme.breakpoints.down("md"));
-  const length = isSmall ? 1 : isMedium ? 2 : 3;
+  const length = isSmall ? 1 : isMedium ? 3 : 4;
 
   return length;
 }
