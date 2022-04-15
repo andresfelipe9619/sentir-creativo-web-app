187b3777974cf35a87133740ada35a53819916b4
Andres Suarez <andresfelipe9619@gmail.com>
2022-04-14 22:39:14 -0500

    :sparkles: Add city to dashboard

--- client/src/components/dashboard/proyecto/columns.js
+++ client/src/components/dashboard/proyecto/columns.js
@@ -109,6 +109,18 @@ const columns = [
     },
   },
   {
+    name: "ciudad",
+    label: "Ciudad",
+    options: {
+      filter: true,
+      sort: true,
+    },
+    form: {
+      size: 12,
+      type: "city",
+    },
+  },
+  {
     name: "formatos",
     label: "Formatos",
     options: {
