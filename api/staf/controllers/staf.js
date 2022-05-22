8e00a3f5ec0fc666d7b72f00a59707ada203b9b1
Jeferson A. Bernabel <jefersonbc000@gmail.com>
2022-05-22 11:19:03 -0400

    create new staf (no working)

--- api/staf/controllers/staf.js
+++ api/staf/controllers/staf.js
@@ -55,4 +55,75 @@ module.exports = {
     const entity = await strapi.services.staf.findOne({ id }, populate);
     return sanitizeEntity(entity, { model: strapi.models.staf });
   },
+  async new(ctx) {
+    const { request } = ctx
+    let {
+      prefijo,
+      nombre,
+      apellido,
+      fechaNacimiento,
+      nacionalidad,
+      nombreArtistico,
+      pais,
+      celular,
+      email,
+      coupon,
+      tecnicas,
+      oficio,
+      comentarios = [],
+    } = request.body
+
+    try {
+      console.log(`BODY: `, request.body)
+      if (!email) throw new Error('No hay Email para empezar proyecto')
+      const knex = strapi.connections.default
+
+      const staf = await strapi.services.staf.create({
+        nombre,
+        apellido,
+        nombreArtistico,
+        oficio,
+        nacionalidad,
+        email,
+        celular,
+        tecnica_artisticas: tecnicas,
+        fechaNacimiento,
+        cuponDescuento: [coupon],
+        prefijo,
+        destacado: false,
+        ciudad: pais,
+        origen: 1,
+        roles: [6],
+        estado: 4
+      });
+
+      await Promise.all(comentarios.map(async comentario => {
+        const comment = await strapi.services.comentario.create({ comentario })
+        await knex.transaction(async trx => {
+          await trx('stafs__comentarios').insert([
+            {
+              proyecto_id: staf.id,
+              comentario_id: comment.id
+            }
+          ])
+        })
+      }));
+
+      return staf;
+    } catch (error) {
+      console.error(error)
+      return ctx.throw(500, error.toString())
+    }
+  },
+  async addFiles (ctx) {
+    const { request, params } = ctx
+    const { id } = params
+    const { body: files } = request
+    const result = await strapi.services.archivo.addFiles({
+      id,
+      files,
+      collection: 'staf'
+    })
+    return result
+  },
 };
