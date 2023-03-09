f56c11a786a94ee9f89e83d2e33ab964c6c275ad
DeveloperSentirCreativo <developer@sentircreativo.com>
2023-03-08 23:07:30 -0300

    primer cambio de prueba 4

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -22,7 +22,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://sentircreativo.com/">
-        Sentir Creativo
+        Sentir Creativos
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
